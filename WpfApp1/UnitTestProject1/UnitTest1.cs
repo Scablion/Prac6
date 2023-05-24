1c134cdc22f2b3594b0499edb670f1994caa3eaa
Scablion <kashcada24@gmail.com>
2023-05-24 10:57:12 +0300

    add unit test in first excircise

---
+++ WpfApp1/UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using static WpfApp1.Class1;
+
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            int a = 123;
+            int actual = fx(a);
+            int exeption = 2;
+            Assert.AreEqual(actual, exeption);
+        }
+        [TestMethod]
+        public void TestMethod2()
+        {
+            int a = 38308;
+            int actual = fx(a);
+            int exeption = 0;
+            Assert.AreEqual(actual, exeption);
+        }
+        [TestMethod]
+        public void TestMethod3()
+        {
+            int a = 6788;
+            int actual = fx(a);
+            int exeption = 8;
+            Assert.AreEqual(actual, exeption);
+        }
+    }
+}
