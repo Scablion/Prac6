62a17642949b05259bbada9064f35b2f92b7c659
Scablion <kashcada24@gmail.com>
2023-05-26 13:56:26 +0300

    dop1

--- Dop1/UnitTestProject1/UnitTest1.cs
+++ Dop1/UnitTestProject1/UnitTest1.cs
@@ -11,29 +11,29 @@ namespace UnitTestProject1
         public void Zadanie1_1()
         {
             int a = 5;
-            int b = 10;
-            int c = 2;
-            double exp = 3;
+            int b = 7;
+            int c = 8;
+            double exp = 17.3205080756888;
             double act = Fxyz(a, b, c);
             Assert.AreEqual(exp, act);
         }
         [TestMethod]
         public void Zadanie1_2()
         {
-            int a = 5;
-            int b = 10;
-            int c = 2;
-            double exp = 2;
+            int a = 8;
+            int b = 8;
+            int c = 5;
+            double exp = 14.142135623731;
             double act = Fxyz(a, b, c);
             Assert.AreEqual(exp, act);
         }
         [TestMethod]
         public void DopZadanie1_3()
         {
-            int a = 5;
-            int b = 10;
-            int c = 2;
-            double exp = -0.5;
+            int a = 6;
+            int b = 4;
+            int c = 8;
+            double exp = 11.6189500386223;
             double act = Fxyz(a, b, c);
             Assert.AreEqual(exp, act);
         }
